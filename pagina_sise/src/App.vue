7cdadc59c6e89796d93673c9138668a16af55702
Madnass92 <yesowrk@gmail.com>
2023-08-29 22:39:42 -0600

    first commit

---
+++ pagina_sise/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import PreloaderView from './modules/preloader/views/PreloaderView.vue';
+  export default{
+      components: {
+        PreloaderView
+      }  }
+</script>
+
+<template >
+  <div>
+    <PreloaderView/>
+  </div>
+  
+
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ pagina_sise/src/modules/preloader/views/PreloaderView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <center>
+            <img src="../../../assets/Logo.png">
+        <LoaderComponent v-if="desactivarLoader"/>
+        <h1 v-else>Loader desactivador</h1>
+        </center>
+        
+    </div>
+    
+</template>
+<script>
+import LoaderComponent from '../components/LoaderComponent.vue';
+
+export default{
+    components: {
+        LoaderComponent,
+    },
+    data(){
+        return {
+            desactivarLoader: true,
+        }
+    },
+
+    methods:{
+        quitarLoader(){
+
+            setTimeout(()=>{
+               let respuesta =  fetch('https://www.aseguradoraabank.com/', 'GET');
+                console.log(respuesta)
+               if(respuesta.length > 0){
+                this.desactivarLoader = false;
+               }
+            
+            }, 2000)
+            
+        },
+    
+        
+    },
+
+mounted(){
+    this.quitarLoader();
+}    
+    
+}
+</script>
+
+<style>
+
+</style>
